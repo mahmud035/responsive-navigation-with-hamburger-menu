02fdced4631e7a92d0d56d7280d082487940e686
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-08 08:59:24 +0600

    sample responsive navigation with hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+const listItems = document.querySelectorAll('.list-item');
+console.log(listItems);
+
+hamburger.addEventListener('click', function () {
+  navUL.classList.toggle('show');
+});
+
+listItems.forEach((item) => {
+  // console.log(item);
+  item.addEventListener('click', function () {
+    navUL.classList.remove('show');
+  });
+});
